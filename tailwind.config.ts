1673b8bb2035446b609cd7f22eb11990feaf032d
saban942025-cyber <saban942025@gmail.com>
2025-12-31 05:57:42 +0200

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,15 +7,7 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-      fontFamily: {
-        sans: ['var(--font-rubik)', 'sans-serif'],
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
